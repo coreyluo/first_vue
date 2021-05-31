e8bc8247e4dc531aed4671e2a297ec896e23e3a6
hubingliang <511632242@qq.com>
2021-05-31 20:50:31 +0800

    dragon 修改

--- src/page/userLogin.vue
+++ src/page/userLogin.vue
@@ -28,6 +28,7 @@
             <MenuItem  name="1-6"><router-link to="/content/1"><font color="#fff">参数</font></router-link></MenuItem>
             <MenuItem  name="1-7"><router-link to="/sellOrder/1"><font color="#fff">卖出</font></router-link></MenuItem>
             <MenuItem  name="1-9"><router-link to="/openButton/1"><font color="#fff">按钮</font></router-link></MenuItem>
+            <MenuItem  name="1-10"><router-link to="/blockView/1"><font color="#fff">板块信息</font></router-link></MenuItem>
           </Submenu>
         </Menu>
       </Sider>
