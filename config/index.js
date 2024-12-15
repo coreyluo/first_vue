0da56220257a3a0586a04189a448c8dad70eedc6
hubingliang <511632242@qq.com>
2024-12-16 00:03:40 +0800

    前端显示深圳上海市场

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/**/**': {
-            target: 'http://183.234.94.164:49503', // 你接口的域名
+            target: 'http://125.93.72.195:5381', // 你接口的域名
             secure: false,
             changeOrigin: true,
             pathRewrite:{
